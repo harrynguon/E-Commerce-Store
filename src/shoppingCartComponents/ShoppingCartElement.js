1ee65e150a71146c9e9c110f1494aeaab885ab67
Harry Nguon <harrynguon@gmail.com>
2019-09-09 03:31:43 +1200

    Added toast for removing item from cart

--- src/shoppingCartComponents/ShoppingCartElement.js
+++ src/shoppingCartComponents/ShoppingCartElement.js
@@ -3,13 +3,28 @@ import { connect } from 'react-redux';
 
 import { deleteFromCart } from '../actions/deleteFromCart';
 
+import M from 'materialize-css';
+
+const showToast = (itemName) => {
+    M.toast(
+        {
+            html: `${itemName} have been removed from your cart!`,
+            displayLength: '1500'
+        }
+    );
+}
+
 const ShoppingCartElement = (props) => {
     const shoppingCart = props.shoppingCart;
     const allShoppingItems = shoppingCart.map(item =>
         <li className="collection-item" key={item.name}>
             <p>
                 { item.name }
-                <button onClick={() => props.deleteFromCart(item.name)}
+                <button onClick={() =>  {
+                                            showToast(item.name)
+                                            props.deleteFromCart(item.name);
+                                        }
+                                }
                         className="btn-flat secondary-content"
                 >
                     <i className="material-icons black-text">clear</i>
