63aad3c9c13244267b35411ae116d3d6e1c754e8
Harry Nguon <harrynguon@gmail.com>
2019-09-09 21:19:08 +1200

    Removed specific React:HTML tag warnings

--- src/shoppingCartComponents/Checkout.js
+++ src/shoppingCartComponents/Checkout.js
@@ -17,59 +17,59 @@ class Checkout extends React.Component {
             <div className="container">
                 <div className="flow-text">Checkout</div>
                 <div className="section"></div>
-                <div class="row">
-                    <form class="col s12">
-                        <div class="row">
-                            <div class="input-field col s6">
-                                <input id="first_name" type="text" class="validate"></input>
-                                <label for="first_name">First Name</label>
+                <div className="row">
+                    <form className="col s12">
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input id="first_name" type="text"></input>
+                                <label htmlFor="first_name">First Name</label>
                             </div>
-                            <div class="input-field col s6">
-                                <input id="last_name" type="text" class="validate"></input>
-                                <label for="last_name">Last Name</label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input placeholder="House number and street name" type="text" class="validate"></input>
-                                <label for="first_name">Street Address</label>
-                            </div>
-                            <div class="input-field col s12">
-                                <input placeholder="Apt, suite, etc." type="text" class="validate"></input>
+                            <div className="input-field col s6">
+                                <input id="last_name" type="text"></input>
+                                <label htmlFor="last_name">Last Name</label>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input type="text" class="validate"></input>
-                                <label for="first_name">Town / City</label>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input type="text"></input>
+                                <label htmlFor="first_name">Street Address</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input placeholder="Apt, suite, etc." type="text"></input>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input type="text" class="validate"></input>
-                                <label for="first_name">Country</label>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input type="text"></input>
+                                <label htmlFor="first_name">Town / City</label>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input type="number" class="validate"></input>
-                                <label for="first_name">ZIP</label>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input type="text" ></input>
+                                <label htmlFor="first_name">Country</label>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input type="tel" class="validate"></input>
-                                <label for="first_name">Phone Number</label>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input type="number" ></input>
+                                <label htmlFor="first_name">ZIP</label>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input id="email" type="email" class="validate"></input>
-                                <label for="email">Email</label>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input type="tel"></input>
+                                <label htmlFor="first_name">Phone Number</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input id="email" type="email"></input>
+                                <label htmlFor="email">Email</label>
                             </div>
                         </div>
                         
-                        <div class="row">
+                        <div className="row">
                             <div>
                                 <Link to="/shoppingcart">
                                     <button className="btn-flat left">Go Back</button>
